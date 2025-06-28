03f5e6ee975fcb62602081679e9c8cea3a09a3c4
microgeniusz80 <79121880+microgeniusz80@users.noreply.github.com>
2025-06-29 00:40:46 +0800

    changed the exoress version

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.get('/api/icd11search', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Proxy server running on port ${PORT}`);
 });
